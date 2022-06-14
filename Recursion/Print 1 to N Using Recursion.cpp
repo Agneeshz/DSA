1e3124d6b16ce1a8502a2c98e8b26c593423a9d9
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-06-15 01:06:57 +0530

    done

--- Recursion/Print 1 to N Using Recursion.cpp
+++ Recursion/Print 1 to N Using Recursion.cpp
@@ -5,12 +5,16 @@ void Print1toN(int n)
 {
     if(n==0)
         return;
-    Print1toN(n);
+    Print1toN(n-1);
     cout<<n<<" ";
 }
 
 int main()
 {
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
     int n;
     cin>>n;
     Print1toN(n);
