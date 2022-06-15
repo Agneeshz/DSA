b51c29921a79e96404fec7a92c3591566a6cefc4
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-06-16 00:35:55 +0530

    Fibonacci

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSum(int n)
+{
+    if(n==0)
+        return 0;
+    return getSum(n-1)+getSum(n-2);
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n=4;;
+    cout<<"getSum(n)";
+    return 0;
+}
