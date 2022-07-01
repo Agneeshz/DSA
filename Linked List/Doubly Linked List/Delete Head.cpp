978f94533a8737f4736a0654c142faa6177e9d44
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-02 02:39:37 +0530

    stack

---
+++ Linked List/Doubly Linked List/Delete Head.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int x)
+    {
+        x = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+Node *DeleteHead(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    Node *temp = head;
+    head = head->next;
+    head->prev = NULL;
+    delete temp;
+    return head;
+}
+
+int main()
+{
+    return 0;
+}
---
+++ Linked List/Doubly Linked List/Delete Last Node.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int x)
+    {
+        x = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+Node *DeleteLast(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    Node *curr = head;
+    while (curr->next != NULL)
+        curr = curr->next;
+    curr->prev->next = NULL;
+    delete curr;
+}
+
+int main()
+{
+
+    return 0;
+}
