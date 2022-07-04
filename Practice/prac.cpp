9faa6b142d247e0adefb40c0b402e77a017b6d80
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-04 22:32:01 +0530

    made linked list

---
+++ Practice/prac.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    Node *next;
+    int data;
+    Node(int x)
+    {
+        x = data;
+        next = NULL;
+    }
+};
+
+int main()
+{
+
+    
+}
