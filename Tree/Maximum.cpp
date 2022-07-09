083c4e7bfca6e60fb8142b0951c2a90e742e97a3
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-10 03:27:24 +0530

    tree

---
+++ Tree/Maximum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int getMax(Node *root)
+{
+    if(root == NULL)
+        return INT_MIN;
+    return max(root->key,max(getMax(root->left),getMax(root->right)));
+}
+
+int main()
+{
+
+    return 0;
+}
