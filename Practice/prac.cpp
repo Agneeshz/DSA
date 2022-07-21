d98232b3a5971d4274576cb3ae6af497ccfef8cc
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-21 14:16:23 +0530

    done

--- Practice/prac.cpp
+++ Practice/prac.cpp
@@ -1,19 +1,8 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-struct Node
-{
-    Node *next;
-    int data;
-    Node(int x)
-    {
-        x = data;
-        next = NULL;
-    }
-};
 
 int main()
 {
-
+    
     return 0;
 }
