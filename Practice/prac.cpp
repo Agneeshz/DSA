7ee686aa7c65d08bc2bdd9fd34d412d6081ddb4b
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-21 14:22:22 +0530

    practice

--- Practice/prac.cpp
+++ Practice/prac.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main()
 {
-    
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    int a=1,b=2;
+    cout<<a+b;
+    cerr<<"time taken: "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
     return 0;
 }
