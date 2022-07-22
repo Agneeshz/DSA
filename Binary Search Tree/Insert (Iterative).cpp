e6d442f5ba9273753deb2a8f6688e28a523588fc
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-22 21:55:41 +0530

    delete

---
+++ Binary Search Tree/Insert (Iterative).cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left, *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+Node *insert(Node *root, int x)
+{
+    Node *temp = new Node(x);
+    Node *parent = NULL, *curr = root;
+    while (curr != NULL)
+    {
+        parent = curr;
+        if (curr->key > x)
+            curr = curr->left;
+        else if (curr->key < x)
+            curr = curr->right;
+        else
+            return root;
+    }
+    if (parent = NULL)
+        return temp;
+    if (parent->key > x)
+        parent->left = temp;
+    else
+        parent->right = temp;
+    return root;
+}
+
+int main()
+{
+
+    return 0;
+}
